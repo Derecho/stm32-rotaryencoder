59765e98c7ce1811a5732e338a5f5bfcce27b80c
Derecho <derecho@sector5d.org>
2016-06-14 20:30:00 +0200

    Changed EXTI code to resemble stm32-h103 example

--- rotaryencoder/rotaryencoder.c
+++ rotaryencoder/rotaryencoder.c
@@ -1,27 +1,35 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/exti.h>
+#include <libopencm3/cm3/nvic.h>
 #include "rotaryencoder.h"
 
 #define ROTENC_PORT GPIOA
 #define ROTENC_RCC RCC_GPIOA
+#define ROTENC_NVIC NVIC_EXTI0_IRQ
 #define ROTENC_A GPIO0
 #define ROTENC_B GPIO1
 #define ROTENC_EXTI EXTI0
 
+// TODO Try a different GPIO port
+
+void exti0_isr(void)
+{
+    // TODO
+
+    exti_reset_request(ROTENC_EXTI);
+}
+
 void rotenc_setup(void)
 {
 
-    // Used https://www.reddit.com/r/ECE/comments/4cg293/anyone_use_libopencm3_with_external_interrupts_on/
-    // as reference (STM32F3 code).
-
-    //_disable_interrupts();
-    rcc_periph_clock_enable(ROTENC_RCC /*| RCC_SYSCFG*/);
+    // Prepare pin A, B and the AFIO and NVIC for external interrupts.
+    // Pin A triggers an external interrupt, pin B is read when this happens.
+    rcc_periph_clock_enable(ROTENC_RCC | RCC_AFIO);
+    nvic_enable_irq(ROTENC_NVIC);
     gpio_set_mode(ROTENC_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT,
             ROTENC_A | ROTENC_B);
- 
-    // Pin A triggers an external interrupt, pin B is read when this happens.
-    exti_select_source(ROTENC_EXTI, ROTENC_PORT);  // Needs SYSCFG to work?
+    exti_select_source(ROTENC_EXTI, ROTENC_PORT);
     exti_set_trigger(ROTENC_EXTI, EXTI_TRIGGER_RISING);
     exti_enable_request(ROTENC_EXTI);
 }
