06df23e863e684ade8654109187526d598426158
Derecho <derecho@sector5d.org>
2016-06-14 20:00:00 +0200

    Pin B does not need to trigger an ISR. Pin assignment changed.

--- rotaryencoder/rotaryencoder.c
+++ rotaryencoder/rotaryencoder.c
@@ -5,20 +5,23 @@
 
 #define ROTENC_PORT GPIOA
 #define ROTENC_RCC RCC_GPIOA
-#define ROTENC_GPIOA GPIO1
-#define ROTENC_GPIOB GPIO2
-#define ROTENC_EXTIA EXTI1
-#define ROTENC_EXTIB EXTI2
+#define ROTENC_A GPIO0
+#define ROTENC_B GPIO1
+#define ROTENC_EXTI EXTI0
 
 void rotenc_setup(void)
 {
+
+    // Used https://www.reddit.com/r/ECE/comments/4cg293/anyone_use_libopencm3_with_external_interrupts_on/
+    // as reference (STM32F3 code).
+
     //_disable_interrupts();
     rcc_periph_clock_enable(ROTENC_RCC /*| RCC_SYSCFG*/);
     gpio_set_mode(ROTENC_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT,
-            ROTENC_GPIOA | ROTENC_GPIOB);
+            ROTENC_A | ROTENC_B);
  
-    exti_select_source(ROTENC_EXTIA, ROTENC_PORT);
-    exti_select_source(ROTENC_EXTIB, ROTENC_PORT);
-    exti_set_trigger(ROTENC_EXTIA | ROTENC_EXTIB, EXTI_TRIGGER_RISING);
-    exti_enable_request(ROTENC_EXTIA | ROTENC_EXTIB);
+    // Pin A triggers an external interrupt, pin B is read when this happens.
+    exti_select_source(ROTENC_EXTI, ROTENC_PORT);  // Needs SYSCFG to work?
+    exti_set_trigger(ROTENC_EXTI, EXTI_TRIGGER_RISING);
+    exti_enable_request(ROTENC_EXTI);
 }
